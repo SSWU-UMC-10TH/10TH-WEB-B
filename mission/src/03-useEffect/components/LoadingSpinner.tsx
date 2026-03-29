bbe7a4bd972164e8c85355093ba70174bf5e5b8b
김채연 <chaeyeon@chaeyeon-MacBook-Pro.local>
2026-03-29 22:36:14 +0900

    [week03]몽모/김채연 미션2 제출

---
+++ mission/src/03-useEffect/components/LoadingSpinner.tsx
@@ -0,0 +1,6 @@
+export const LoadingSpinner = () => {
+    return <div className="size-12 animate-spin rounded-full border-6 
+    border-t-transparent border-[#b2dad1]" role="status">
+        <span className="sr-only">로딩중...</span>
+    </div>
+}
--- mission/src/App.tsx
+++ mission/src/App.tsx
@@ -1,11 +1,31 @@
 import './App.css'
 import { type JSX } from 'react';
 import MoviePage from './03-useEffect/pages/MoviePage';
+import HomePage from './03-useEffect/pages/HomePage';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import NotFoundPage from './03-useEffect/pages/NotFoundPage';
+import MovieDetailPage from './03-useEffect/pages/MovieDetailPage';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <HomePage/>,
+    errorElement: <NotFoundPage/>,
+    children: [
+      {
+        path: 'movies/:category',
+        element: <MoviePage/>,
+      },
+      {
+        path: 'movies/:movieId',
+        element: <MovieDetailPage/>
+      }
+    ]
+  }
+])
 
 function App(): JSX.Element {
-  return (
-    <MoviePage/>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App
