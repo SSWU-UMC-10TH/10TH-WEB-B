fe59d692be9552943cd0fbdf462c7bd73795ced1
김채연 <chaeyeon@chaeyeon-MacBook-Pro.local>
2026-03-29 19:16:00 +0900

    [week03]몽모/김채연 미션1 수정제출

--- mission/src/App.tsx
+++ mission/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { type JSX } from 'react';
-import MoviePage from './pages/MoviePage';
+import MoviePage from './03-useEffect/pages/MoviePage';
 
 function App(): JSX.Element {
   return (
