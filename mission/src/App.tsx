094cae865a89eb0fb03e1b86e32afc214d2f3ce0
김채연 <chaeyeon@chaeyeon-MacBook-Pro.local>
2026-03-29 19:14:09 +0900

    [week03]몽모/김채연 미션1 제출

---
+++ mission/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { type JSX } from 'react';
+import MoviePage from './pages/MoviePage';
+
+function App(): JSX.Element {
+  return (
+    <MoviePage/>
+  );
+};
+
+export default App
