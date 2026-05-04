9aed685efa6cc876b670d8c75f3ce29b944a6406
DaAn <andh0812@naver.com>
2026-05-04 18:34:27 +0900

    feat : 플로팅 버튼 수정

--- layouts/HomeLayout.tsx
+++ layouts/HomeLayout.tsx
@@ -9,6 +9,26 @@ const HomeLayout = () => {
         </main>
             
         <footer>푸터</footer>
+        {/* 플로팅 버튼 */}
+      <button
+        onClick={() => navigate("/lp/new")} // 이동 경로
+        style={{
+          position: "fixed",
+          bottom: "30px",
+          right: "30px",
+          width: "60px",
+          height: "60px",
+          borderRadius: "50%",
+          backgroundColor: "#ff5a5f",
+          color: "white",
+          fontSize: "30px",
+          border: "none",
+          cursor: "pointer",
+          boxShadow: "0 4px 12px rgba(0,0,0,0.3)",
+        }}
+      >
+        +
+      </button>
     </div>
     );
   
