7d945c2d2caa139efc84a93bf2ee861a26920bb4
Soo411 <Simsooa12@gmail.com>
2026-05-27 04:13:24 +0900

    chapter09 mission01

---
+++ Chapter09/mission01/src/components/PriceBox.tsx
@@ -0,0 +1,22 @@
+import { useDispatch, useSelector } from "../hooks/useCustomRedux";
+import { clearCart } from "../slices/cartSlice";
+
+const PriceBox = () => {
+    const { total } = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    const handleInitializeCart = () => {
+        dispatch(clearCart());
+    };
+
+    return (
+        <div className="mx-auto w-full max-w-3xl px-4 pt-2 pb-12 flex flex-col items-center gap-6">
+            <p className="self-end">총 가격 {total} 원</p>
+            <button onClick={handleInitializeCart} className="border p-4 rounded-md cursor-pointer">
+                전체 삭제
+            </button>
+        </div>
+    );
+};
+
+export default PriceBox;
