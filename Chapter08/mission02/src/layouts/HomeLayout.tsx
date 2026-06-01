e8510b056284767664ef16c7524b8f4f1f154f02
Soo411 <Simsooa12@gmail.com>
2026-06-01 23:43:14 +0900

    chapter10 mission01

--- Chapter08/mission02/src/layouts/HomeLayout.tsx
+++ Chapter08/mission02/src/layouts/HomeLayout.tsx
@@ -1,17 +1,17 @@
-import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/Navbar";
 import Footer from "../components/Footer";
 import Sidebar from "../components/Sidebar";
 import FloatingCreateLpButton from "../components/FloatingCreateLpButton";
+import useSidebar from "../hooks/useSidebar";
 
 const HomeLayout = () => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const { isOpen, toggle, close } = useSidebar();
 
     return (
         <div className="min-h-dvh bg-black">
-            <Navbar onToggleSidebar={() => setIsSidebarOpen((prev) => !prev)} />
-            <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+            <Navbar onToggleSidebar={toggle} />
+            <Sidebar isOpen={isOpen} onClose={close} />
             <main className="min-h-dvh pt-16 md:pl-40">
                 <Outlet />
             </main>
