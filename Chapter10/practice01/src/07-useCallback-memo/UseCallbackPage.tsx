ce95960683ffcd130b7893ca67b8de1d515f3d57
Soo411 <Simsooa12@gmail.com>
2026-06-06 06:01:21 +0900

    chapter10 실습1

---
+++ Chapter10/practice01/src/07-useCallback-memo/UseCallbackPage.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useState } from "react";
+import CountButton from "./components/CountButton";
+import TextInput from "./components/TextInput";
+
+export default function UseCallbackPage() {
+    const [count, setCount] = useState<number>(0);
+    const [text, setText] = useState<string>("");
+
+    const handleIncreaseCount = useCallback(
+        (number: number) => {
+            setCount(count + number);
+        },
+        [count],
+    );
+
+    const handleText = useCallback((text: string) => {
+        setText(text);
+    }, []);
+
+    return (
+        <div>
+            <h1>같이 배우는 리액트 useCallback편</h1>
+            <h2>Count : {count}</h2>
+            <CountButton onClick={handleIncreaseCount} />
+            <h2>Text</h2>
+            <span>{text}</span>
+            <TextInput onChange={handleText} />
+        </div>
+    );
+}
